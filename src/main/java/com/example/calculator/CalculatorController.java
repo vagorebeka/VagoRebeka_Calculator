c3a976178872d77c1235a98a4045a2b53566df59
Vágó Rebeka <79878482+vagorebeka@users.noreply.github.com>
2023-04-19 01:47:13 +0200

    Feladat kész

--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -1,14 +1,86 @@
 package com.example.calculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.text.DecimalFormat;
+import java.util.Optional;
 
 public class CalculatorController {
-    @FXML
-    private Label welcomeText;
+    public TextField szam1Field;
+    public TextField szam2Field;
+    public Label eredmenyLabel;
+    double szam1;
+    double szam2;
+    Double eredmeny;
+    DecimalFormat df = new DecimalFormat("#.##");
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public boolean kitoltottEllenoriz(){
+        boolean kitoltve = false;
+        if (szam1Field.getText().trim() == "" || szam2Field.getText().trim() == ""){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Nem lett szám megadva!", ButtonType.OK);
+            alert.showAndWait();
+        }else{
+            try {
+                szam1 = Double.parseDouble(szam1Field.getText());
+                szam2 = Double.parseDouble(szam2Field.getText());
+                kitoltve = true;
+            }catch(Exception e){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Csak számot adjon meg!", ButtonType.OK);
+                alert.showAndWait();
+            }
+        }
+        return kitoltve;
+    }
+
+    public void pluszButtonClick(ActionEvent actionEvent) {
+        if (kitoltottEllenoriz()){
+            szam1 = Double.parseDouble(szam1Field.getText());
+            szam2 = Double.parseDouble(szam2Field.getText());
+            eredmeny = szam1 + szam2;
+            eredmenyLabel.setText(String.valueOf(eredmeny));
+        }
+    }
+
+    public void minuszButtonClick(ActionEvent actionEvent) {
+        if (kitoltottEllenoriz()){
+        szam1 = Double.parseDouble(szam1Field.getText());
+        szam2 = Double.parseDouble(szam2Field.getText());
+        eredmeny = szam1 - szam2;
+        eredmenyLabel.setText(String.valueOf(eredmeny));
+        }
+    }
+
+    public void szorzasButtonClick(ActionEvent actionEvent) {
+        if (kitoltottEllenoriz()){
+        szam1 = Double.parseDouble(szam1Field.getText());
+        szam2 = Double.parseDouble(szam2Field.getText());
+        eredmeny = szam1 * szam2;
+        eredmenyLabel.setText(String.valueOf(eredmeny));
+        }
+    }
+
+    public void osztasButtonClick(ActionEvent actionEvent) {
+        if (kitoltottEllenoriz()){
+        szam1 = Double.parseDouble(szam1Field.getText());
+        szam2 = Double.parseDouble(szam2Field.getText());
+        eredmeny = szam1 / szam2;
+        eredmenyLabel.setText(String.valueOf(df.format(eredmeny)));
+        }
+    }
+
+    public void szazalekButtonClick(ActionEvent actionEvent) {
+        if (kitoltottEllenoriz()){
+        szam1 = Double.parseDouble(szam1Field.getText());
+        szam2 = Double.parseDouble(szam2Field.getText());
+        eredmeny = szam1 / szam2 * 100;
+        eredmenyLabel.setText(df.format(eredmeny) + "%");
+        }
     }
 }
